484201750b096dc6198c484440223589e6ae4664
masahiro-yamashita <masahiro.yamashita@linecorp.com>
2022-01-23 01:46:03 +0900

    Added reactor-test

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core")
     testImplementation("ch.qos.logback:logback-classic")
+    testImplementation("io.projectreactor:reactor-test")
 }
