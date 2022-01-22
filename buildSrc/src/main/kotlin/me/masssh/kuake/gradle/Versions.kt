277b227e76a5b5181f44f12ae1037588a741a5a0
masahiro-yamashita <masahiro.yamashita@linecorp.com>
2022-01-23 01:28:37 +0900

    Added r2dbc-spi dependency

--- buildSrc/src/main/kotlin/me/masssh/kuake/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/me/masssh/kuake/gradle/Versions.kt
@@ -7,7 +7,7 @@ object Versions {
     const val dependencyManagement = "1.0.11.RELEASE"
     const val reactor = "2020.0.15"
     const val kotlinLogging = "2.1.21"
-    const val r2dbcMysql = "0.8.2.RELEASE"
+    const val r2dbc = "0.8.2.RELEASE"
     const val junit = "5.8.2"
     const val assertj = "3.12.0"
     const val logback = "1.2.10"
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm") version me.masssh.kuake.gradle.Versions.kotlin
     id("org.jlleitschuh.gradle.ktlint") version me.masssh.kuake.gradle.Versions.ktlint
     id("io.gitlab.arturbosch.detekt") version me.masssh.kuake.gradle.Versions.detekt
+    id("io.spring.dependency-management") version me.masssh.kuake.gradle.Versions.dependencyManagement
     `java-library`
 }
 
@@ -25,11 +26,27 @@ repositories {
     mavenCentral()
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("io.projectreactor:reactor-bom:${V.reactor}")
+    }
+    dependencies {
+        dependency("io.github.microutils:kotlin-logging:${V.kotlinLogging}")
+        dependency("io.r2dbc:r2dbc-spi:${V.r2dbc}")
+        dependency("dev.miku:r2dbc-mysql:${V.r2dbc}")
+        dependency("org.junit.jupiter:junit-jupiter:${V.junit}")
+        dependency("org.assertj:assertj-core:${V.assertj}")
+        dependency("ch.qos.logback:logback-classic:${V.logback}")
+    }
+}
+
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("io.github.microutils:kotlin-logging:${V.kotlinLogging}")
-    runtimeOnly("dev.miku:r2dbc-mysql:${V.r2dbcMysql}")
-    testImplementation("org.junit.jupiter:junit-jupiter:${V.junit}")
-    testImplementation("org.assertj:assertj-core:${V.assertj}")
-    testImplementation("ch.qos.logback:logback-classic:${V.logback}")
+    implementation("io.github.microutils:kotlin-logging")
+    implementation("io.r2dbc:r2dbc-spi")
+    implementation("io.projectreactor:reactor-core")
+    runtimeOnly("dev.miku:r2dbc-mysql")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("ch.qos.logback:logback-classic")
 }
