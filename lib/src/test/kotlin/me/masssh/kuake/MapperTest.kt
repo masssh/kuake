305975be6794f1a5ac68d8148b95a79578652985
masahiro-yamashita <masahiro.yamashita@linecorp.com>
2022-01-23 12:06:51 +0900

    Should select and get first column

--- lib/src/test/kotlin/me/masssh/kuake/MapperTest.kt
+++ lib/src/test/kotlin/me/masssh/kuake/MapperTest.kt
@@ -1,12 +1,13 @@
 package me.masssh.kuake
 
 import org.junit.jupiter.api.Test
+import reactor.kotlin.test.test
 
 class MapperTest {
     @Test
-    fun test() {
+    fun `should select and get first column`() {
         val mapper = Mapper()
-        val query = "SELECT * FROM table"
-        mapper.execute(query)
+        val query = "SELECT 'foo'"
+        mapper.execute(query).test().expectNext("foo").verifyComplete()
     }
 }
