d112690bbfdf3729dd93475df3c974718d8c60bc
masahiro-yamashita <masahiro.yamashita@linecorp.com>
2022-01-23 12:04:41 +0900

    Execute select and get first column

--- lib/src/main/kotlin/me/masssh/kuake/Mapper.kt
+++ lib/src/main/kotlin/me/masssh/kuake/Mapper.kt
@@ -1,14 +1,32 @@
 package me.masssh.kuake
 
+import io.r2dbc.spi.Connection
+import io.r2dbc.spi.ConnectionFactories
 import mu.KotlinLogging
-
-data class Record(val name: String)
+import reactor.core.publisher.Flux
+import reactor.kotlin.core.publisher.toFlux
 
 class Mapper {
     private val log = KotlinLogging.logger {}
+    private val connectionFactory = ConnectionFactories.get("r2dbc:mysql://testuser:testpass@127.0.0.1:3306/testdb")
 
-    fun execute(query: String): Record {
+    fun execute(query: String): Flux<String> {
         log.info { query }
-        return Record("dummy")
+        return Flux.usingWhen(
+            connectionFactory.create(),
+            { connection: Connection ->
+                connection.createStatement(query)
+                    .execute()
+                    .toFlux()
+                    .flatMap { result ->
+                        result.map { row, _ ->
+                            val column = row.get(0, String::class.java)
+                            log.info { "index=0 value=$column" }
+                            column
+                        }
+                    }
+            },
+            Connection::close
+        )
     }
 }
