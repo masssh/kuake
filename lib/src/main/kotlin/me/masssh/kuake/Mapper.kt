9b6b68a4f798d4b9331f790123a1d9a030f63b10
masahiro-yamashita <masahiro.yamashita@linecorp.com>
2022-01-23 13:47:31 +0900

    Support multi-column select

--- lib/src/main/kotlin/me/masssh/kuake/Mapper.kt
+++ lib/src/main/kotlin/me/masssh/kuake/Mapper.kt
@@ -2,31 +2,43 @@ package me.masssh.kuake
 
 import io.r2dbc.spi.Connection
 import io.r2dbc.spi.ConnectionFactories
+import io.r2dbc.spi.ConnectionFactory
+import io.r2dbc.spi.Row
+import io.r2dbc.spi.RowMetadata
 import mu.KotlinLogging
 import reactor.core.publisher.Flux
 import reactor.kotlin.core.publisher.toFlux
+import kotlin.reflect.KClass
 
 class Mapper {
     private val log = KotlinLogging.logger {}
     private val connectionFactory = ConnectionFactories.get("r2dbc:mysql://testuser:testpass@127.0.0.1:3306/testdb")
 
-    fun execute(query: String): Flux<String> {
+    inline fun <reified T : Any> kClass(): KClass<T> = T::class
+
+    fun execute(query: String): Flux<Map<String, String?>> {
         log.info { query }
-        return Flux.usingWhen(
-            connectionFactory.create(),
-            { connection: Connection ->
-                connection.createStatement(query)
-                    .execute()
-                    .toFlux()
-                    .flatMap { result ->
-                        result.map { row, _ ->
-                            val column = row.get(0, String::class.java)
-                            log.info { "index=0 value=$column" }
-                            column
-                        }
-                    }
-            },
-            Connection::close
-        )
+        return executeQuery(query)
     }
+
+    private fun executeQuery(query: String): Flux<Map<String, String?>> =
+        connectionFactory.use { connection ->
+            connection.createStatement(query)
+                .execute()
+                .toFlux()
+                .flatMap { result ->
+                    result.map { row, rowMetaData -> createColumnMap(row, rowMetaData) }
+                }
+        }
+
+    private fun ConnectionFactory.use(
+        action: (Connection) -> Flux<Map<String, String?>>
+    ): Flux<Map<String, String?>> = Flux.usingWhen(this.create(), action, Connection::close)
+
+    private fun createColumnMap(row: Row, rowMetaData: RowMetadata): Map<String, String?> = rowMetaData
+        .columnNames.associateWith { columnName ->
+            row.get(columnName, String::class.java).also {
+                log.info { "columnName=$columnName value=$it" }
+            }
+        }
 }
--- lib/src/test/kotlin/me/masssh/kuake/MapperTest.kt
+++ lib/src/test/kotlin/me/masssh/kuake/MapperTest.kt
@@ -5,9 +5,16 @@ import reactor.kotlin.test.test
 
 class MapperTest {
     @Test
-    fun `should select and get first column`() {
+    fun `should select and get all columns`() {
         val mapper = Mapper()
-        val query = "SELECT 'foo'"
-        mapper.execute(query).test().expectNext("foo").verifyComplete()
+        val query = "SELECT 'foo' as a, 'bar' as b, 'baz' as c"
+        mapper.execute(query).test()
+            .expectNext(
+                mapOf(
+                    "a" to "foo",
+                    "b" to "bar",
+                    "c" to "baz",
+                )
+            ).verifyComplete()
     }
 }
